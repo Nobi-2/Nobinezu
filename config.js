a52a07aed678ba50e00a7a9e8cfc512e802d761d
ɳσႦιƚα <127091256+Nobi-2@users.noreply.github.com>
2023-09-16 20:05:19 +0530

    config.js

--- config.js
+++ config.js
@@ -17,9 +17,9 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801853262586","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801928502256"];
+global.Owner = ["917000562148","9174493135"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["917000562148","919174493135"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["917000562148"];
 global.OwnerName = "Slasher";
 global.BotName = "XLICON-MD";
 global.packname = "X-2.0 Bot";                             //Do not change.
